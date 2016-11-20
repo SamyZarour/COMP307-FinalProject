d608d810ae5fdfb76fa75bfc3d8af50a13260bb7
samyzarour <zarour.samy@gmail.com>
2016-11-20 09:24:41 -0500

    spider visits each course and prints their url, commented out next page while loop

--- getCourses/getCourses/spiders/course_spider.py
+++ getCourses/getCourses/spiders/course_spider.py
@@ -1,8 +1,8 @@
 import scrapy
 
-path = 'http://www.mcgill.ca/'
 
 class QuotesSpider(scrapy.Spider):
+
     name = "courses"
 
     def start_requests(self):
@@ -13,18 +13,24 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url
         filename = 'courses.html'
         courses = response.xpath("//h4[@class='field-content']/a/@href")
         print '\n\n'
-        
+
+        # Visit each course on the current page
         for course in courses:
-            print course.extract()
-            print '\n'
-        next_page = response.xpath("//a[@title='Go to next page']/@href").extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            course_page = response.urljoin(course.extract())
+            yield scrapy.Request(course_page, callback=self.extractCourse)
+
+        # # Loop through all pages while there is a next button
+        # next_page = response.xpath("//a[@title='Go to next page']/@href").extract_first()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
 
         print '\n\n'
         self.log('Saved file %s' % filename)
+
+    def extractCourse(self, response):
+        print response.url
+        print '\n'
