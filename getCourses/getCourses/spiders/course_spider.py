62d63aa71bc173056cc2e71ef217dd4673a4a55c
samyzarour <zarour.samy@gmail.com>
2016-11-22 11:11:45 -0500

    return a logically structured prerequired courses for each link

--- getCourses/getCourses/spiders/course_spider.py
+++ getCourses/getCourses/spiders/course_spider.py
@@ -1,13 +1,18 @@
-import scrapy
-import re
+import scrapy, re, os
 from scrapy.http import HtmlResponse
 
 
 class QuotesSpider(scrapy.Spider):
 
     name = "courses"
+    filename = "courses.html"
 
     def start_requests(self):
+        try:
+            os.remove(self.filename)
+        except OSError:
+            pass
+
         urls = [
             'http://www.mcgill.ca/study/2016-2017/courses/search/',
         ]
@@ -24,11 +29,11 @@ class QuotesSpider(scrapy.Spider):
             course_page = response.urljoin(course.extract())
             yield scrapy.Request(course_page, callback=self.extractCourse)
 
-        # # Loop through all pages while there is a next button
-        # next_page = response.xpath("//a[@title='Go to next page']/@href").extract_first()
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
+        # Loop through all pages while there is a next button
+        next_page = response.xpath("//a[@title='Go to next page']/@href").extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
         print '\n\n'
         self.log('Saved file %s' % filename)
@@ -43,9 +48,7 @@ class QuotesSpider(scrapy.Spider):
         for note in catalogNotes:
             if 'Prerequisite' in note.extract():
                 print "\n%s :\n" % course_name
-                self.parsePrereqs(note.extract())
-                # for n in note.xpath("a"):
-                #     print n.extract()
+                self.parsePrereqs(str(note.extract()))
                 print '\n'
 
     def parsePrereqs(self, prereqs):
@@ -57,8 +60,9 @@ class QuotesSpider(scrapy.Spider):
             result.append([])
             for o in a.split('or'):
                 hrefFinder = re.compile(r"href=\"[A-Za-z0-9_/-]+\">([A-Za-z0-9_ ]+)<")
-                print str(hrefFinder.findall(o))
-                # for c in HtmlResponse(url="my HTML string", body=str(o)).xpath('a'):
-                #     print c.extract()
-                #     print '\n'
+                for c in hrefFinder.findall(o):
+                    result[-1].append(c)
             i += 1
+
+        with open(self.filename, 'a') as f:
+            f.write("%s\n%s\n\n" % (prereqs, str(result)))
