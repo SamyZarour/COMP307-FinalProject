985f9b555a61c121d64bb8bfacd6c9247c2d1a29
samyzarour <zarour.samy@gmail.com>
2016-11-27 11:04:00 -0500

    Created Majors model in ruby and generated a list of rows to be inserted into the Majors table by using python

--- getCourses/format_json.py
+++ getCourses/format_json.py
@@ -5,23 +5,30 @@ courses_out = {}
 majors_in = {}
 majors_out = {}
 
-with open('courses.json') as outfile:
-	courses_in = json.load(outfile)
-
-with open('majors.json') as outfile:
+with open('majors_formated.json') as outfile:
 	majors_in = json.load(outfile)
 
-for course in courses_in:
-	if course["subj"] not in courses_out : courses_out[course["subj"]] = []
-	courses_out[course["subj"]].append(course)
+with open('majors.txt', 'w') as outfile:
+	for m in majors_in:
+		outfile.write("%s\n" % m.encode('utf-8'))
 
-for major in majors_in:
-	if major["major"] not in majors_out : majors_out[major["major"]] = []
-	majors_out[major["major"]].append(major["course"])
+# with open('courses.json') as outfile:
+# 	courses_in = json.load(outfile)
 
-with open('courses_formated.json', 'w') as outfile:
-	json.dump(courses_out, outfile)
+# with open('majors.json') as outfile:
+# 	majors_in = json.load(outfile)
 
-with open('majors_formated.json', 'w') as outfile:
-	json.dump(majors_out, outfile)
+# for course in courses_in:
+# 	if course["subj"] not in courses_out : courses_out[course["subj"]] = []
+# 	courses_out[course["subj"]].append(course)
+
+# for major in majors_in:
+# 	if major["major"] not in majors_out : majors_out[major["major"]] = []
+# 	majors_out[major["major"]].append(major["course"])
+
+# with open('courses_formated.json', 'w') as outfile:
+# 	json.dump(courses_out, outfile)
+
+# with open('majors_formated.json', 'w') as outfile:
+# 	json.dump(majors_out, outfile)
 
