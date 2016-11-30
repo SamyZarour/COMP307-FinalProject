a43da42f5552d7276a145a2dba8494363dbc3469
samyzarour <zarour.samy@gmail.com>
2016-11-29 19:55:00 -0500

    Failed Saving Major implementation, it seems like the major attribute of user is not readable or writtable. This might be due to adding a column after the User model was already created

--- BluePrint/app/controllers/users_controller.rb
+++ BluePrint/app/controllers/users_controller.rb
@@ -22,9 +22,26 @@ class UsersController < ApplicationController
 
   end
 
+  def update
+    @user = current_user
+    User.all.each do |u|
+      puts u.username
+      debugger(u.major)
+    end
+    if current_user.update_column(:major, params[:user][:major])
+      redirect_to "/profile"
+    else
+      redirect_to "/"
+    end
+  end
+
 private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :major)
+  end
+
+  def major_params
+    params.require(:user).permit(:major)
   end
 end
