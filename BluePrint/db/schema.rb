3922e18cfb94058aad11277baad966d73731ddc2
SamyZarour <zarour.samy@gmail.com>
2016-12-01 04:52:23 +0000

    Fixed navbar links color. Deployed to apache server.

--- BluePrint/db/schema.rb
+++ BluePrint/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20161130224920) do
 
-  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "commenter"
     t.text     "body",       limit: 65535
     t.integer  "course_id"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20161130224920) do
     t.index ["course_id"], name: "index_comments_on_course_id", using: :btree
   end
 
-  create_table "courses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "courses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.string   "faculty"
     t.integer  "crn"
@@ -29,13 +29,13 @@ ActiveRecord::Schema.define(version: 20161130224920) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "majors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "majors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
